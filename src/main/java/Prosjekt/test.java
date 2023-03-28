aa5998720adbd18593fc3daa7bd609e47253b23c
hjalmabj <hjalmabj@stud.ntnu.no>
2023-03-28 15:53:03 +0200

    "test"

---
+++ src/main/java/Prosjekt/test.java
@@ -0,0 +1,8 @@
+package Prosjekt;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("halla vegard");
+        System.out.println("heigit config --global user.name");
+    }
+}
