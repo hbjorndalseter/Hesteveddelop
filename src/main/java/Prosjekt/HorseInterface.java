deb52973b904ec0911469def38807761c6577c24
hjalmabj <hjalmabj@stud.ntnu.no>
2023-03-29 09:30:49 +0200

    Update

--- src/main/java/Prosjekt/HorseInterface.java
+++ src/main/java/Prosjekt/HorseInterface.java
@@ -1,8 +1,5 @@
 package Prosjekt;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public interface HorseInterface {
     
 public void newGame(String...userNames);
@@ -12,6 +9,5 @@ public void runGame();
 public void addWinner();
 
 public void createUsername(String userName);
-
 
 }
