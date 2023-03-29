7e8a9527a02d31fd54f7911038162441ff507698
vegano <vegano@stud.ntnu.no>
2023-03-29 09:30:58 +0200

    suit

--- src/main/java/Prosjekt/HorseGameController.java
+++ src/main/java/Prosjekt/HorseGameController.java
@@ -1,5 +1,22 @@
 package Prosjekt;
 
+import java.util.ArrayList;
+
 public class HorseGameController {
     
+    public ArrayList<Integer> raceLocation; 
+    private ArrayList<Card> cards;
+
+
+    public HorseGameController() {
+        ArrayList<Integer> raceLocation = new ArrayList<Integer>();
+
+    }
+
+    public void goForward(){
+        this.card = pickRandomCard().getSuit()
+    }
+
+
+
 }
