84135f3e24831640020d302b694c3fbc0d75f160
hjalmabj <hjalmabj@stud.ntnu.no>
2023-04-10 09:17:13 +0200

    h

--- src/main/java/Prosjekt/HorseGameController.java
+++ src/main/java/Prosjekt/HorseGameController.java
@@ -6,6 +6,9 @@ import java.util.HashMap;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
 public class HorseGameController {
     
@@ -20,10 +23,26 @@ public class HorseGameController {
     private Button newCardButton;
 
     HorseGame game;
+    GridPane gridPane;
+    ImageView Diamonds;
+    ImageView Hearts;
+    ImageView Spades;
+    ImageView Clubs;
 
     public HorseGameController() {
         HashMap<String, Integer> raceLocation = new HashMap<String, Integer>();
         this.game = new HorseGame();
+        this.gridPane = new GridPane();
+        this.Diamonds= new ImageView("@../../../../../../Skjermbilde%202023-03-30%20kl.%2010.15.05.png");
+        this.Hearts = new ImageView("@../../../../../../Skjermbilde%202023-03-30%20kl.%2010.15.00.png");
+        this.Spades = new ImageView("@../../../../../../Skjermbilde%202023-03-30%20kl.%2010.14.41.png");
+        this.Clubs = new ImageView("@../../../../../../Skjermbilde%202023-03-30%20kl.%2010.13.54.png");
+        gridPane.getChildren().add(Diamonds);
+        gridPane.getChildren().add(Hearts);
+        gridPane.getChildren().add(Spades);
+        gridPane.getChildren().add(Clubs);
+
+        
         
     }
 
@@ -39,4 +58,16 @@ public class HorseGameController {
         ///game.pickRandomCard();
         System.out.println(game.pickRandomCard());
 }
+    @FXML 
+    private void setStartPos(){
+        GridPane.setColumnIndex(Diamonds, 1);
+        GridPane.setColumnIndex(Hearts, 1);
+        GridPane.setColumnIndex(Spades, 1);
+        GridPane.setColumnIndex(Clubs, 1);
+        GridPane.setRowIndex(Diamonds, 1);
+        GridPane.setRowIndex(Hearts, 2);
+        GridPane.setRowIndex(Spades, 3);
+        GridPane.setRowIndex(Clubs, 4);
+    }
+
 }
