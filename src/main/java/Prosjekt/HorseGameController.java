e2dccabd18bb3aede762dfd60f9e8c1b7b3dd2b0
hjalmabj <hjalmabj@stud.ntnu.no>
2023-04-11 14:16:41 +0200

    h

--- src/main/java/Prosjekt/HorseGameController.java
+++ src/main/java/Prosjekt/HorseGameController.java
@@ -36,6 +36,7 @@ public class HorseGameController {
     Image pokemonCardSixth;
     Image pokemonCardSeventh;
     Image pokemonCardEight;
+    
 
     public HorseGameController() {
         HashMap<String, Integer> raceLocation = new HashMap<String, Integer>();
@@ -55,6 +56,15 @@ public class HorseGameController {
         this.pokemonCardEight = new Image("Pokemon.png");
     
         this.gridPane.getChildren().add(new ImageView(Diamonds));
+        ImageView diamonds = new ImageView(Diamonds);
+        ImageView hearts = new ImageView(Hearts);
+        ImageView spades = new ImageView(Spades);
+        ImageView clubs = new ImageView(Clubs);
+        this.gridPane.add(diamonds, 1, 0);
+        this.gridPane.add(hearts, 1, 1);
+        this.gridPane.add(spades, 1, 2);
+        this.gridPane.add(clubs, 1, 3);
+
 
         
 
@@ -81,36 +91,36 @@ public class HorseGameController {
         ///game.pickRandomCard();
         System.out.println(game.pickRandomCard());
 }
-    @FXML 
-    private void setStartPos(){
-        GridPane.setColumnIndex(Diamonds, 1);
-        GridPane.setColumnIndex(Hearts, 1);
-        GridPane.setColumnIndex(Spades, 1);
-        GridPane.setColumnIndex(Clubs, 1);
+    // @FXML 
+    // private void setStartPos(){
+    //     GridPane.setColumnIndex(diamonds, 1);
+    //     GridPane.setColumnIndex(Hearts, 1);
+    //     GridPane.setColumnIndex(Spades, 1);
+    //     GridPane.setColumnIndex(Clubs, 1);
 
-        GridPane.setRowIndex(Diamonds, 1);
-        GridPane.setRowIndex(Hearts, 2);
-        GridPane.setRowIndex(Spades, 3);
-        GridPane.setRowIndex(Clubs, 4);
+    //     GridPane.setRowIndex(Diamonds, 1);
+    //     GridPane.setRowIndex(Hearts, 2);
+    //     GridPane.setRowIndex(Spades, 3);
+    //     GridPane.setRowIndex(Clubs, 4);
         
-        GridPane.setColumnIndex(pokemonCardFirst, 2);
-        GridPane.setColumnIndex(pokemonCardSecond, 2);
-        GridPane.setColumnIndex(pokemonCardThird, 2);
-        GridPane.setColumnIndex(pokemonCardFourth, 2);
-        GridPane.setColumnIndex(pokemonCardFifth, 2);
-        GridPane.setColumnIndex(pokemonCardSixth, 2);
-        GridPane.setColumnIndex(pokemonCardSeventh, 2);
-        GridPane.setColumnIndex(pokemonCardEight, 2);
+    //     GridPane.setColumnIndex(pokemonCardFirst, 2);
+    //     GridPane.setColumnIndex(pokemonCardSecond, 2);
+    //     GridPane.setColumnIndex(pokemonCardThird, 2);
+    //     GridPane.setColumnIndex(pokemonCardFourth, 2);
+    //     GridPane.setColumnIndex(pokemonCardFifth, 2);
+    //     GridPane.setColumnIndex(pokemonCardSixth, 2);
+    //     GridPane.setColumnIndex(pokemonCardSeventh, 2);
+    //     GridPane.setColumnIndex(pokemonCardEight, 2);
 
-        GridPane.setRowIndex(pokemonCardFirst, 2);
-        GridPane.setRowIndex(pokemonCardSecond, 3);
-        GridPane.setRowIndex(pokemonCardThird, 4);
-        GridPane.setRowIndex(pokemonCardFourth, 5);
-        GridPane.setRowIndex(pokemonCardFifth, 6);
-        GridPane.setRowIndex(pokemonCardSixth, 7);
-        GridPane.setRowIndex(pokemonCardSeventh, 8);
-        GridPane.setRowIndex(pokemonCardEight, 9);
+    //     GridPane.setRowIndex(pokemonCardFirst, 2);
+    //     GridPane.setRowIndex(pokemonCardSecond, 3);
+    //     GridPane.setRowIndex(pokemonCardThird, 4);
+    //     GridPane.setRowIndex(pokemonCardFourth, 5);
+    //     GridPane.setRowIndex(pokemonCardFifth, 6);
+    //     GridPane.setRowIndex(pokemonCardSixth, 7);
+    //     GridPane.setRowIndex(pokemonCardSeventh, 8);
+    //     GridPane.setRowIndex(pokemonCardEight, 9);
 
-    }
+    // }
 
 }
